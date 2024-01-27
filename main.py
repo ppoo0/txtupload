f7aa9e2e60c6dc7a174dde9c5e3d6b91bd060463
Theamitkumarsaini <145679072+Theamitkumarsaini@users.noreply.github.com>
2024-01-27 12:18:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def account_login(bot: Client, m: Message):
 
 @bot.on_message(filters.command("stop")&(filters.chat(auth_users)))
 async def restart_handler(_, m):
-    await m.reply_text("**KISHAN**🛑", True)
+    await m.reply_text("**STOPPED**🛑", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 

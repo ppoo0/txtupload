1ae37f64c4122dbc8eb60b250059f79b4169c009
Theamitkumarsaini <145679072+Theamitkumarsaini@users.noreply.github.com>
2024-01-27 12:37:24 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -66,7 +66,7 @@ def hrt(seconds, precision = 0):
 
 timer = Timer()
 
-# 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝘽𝙮 
+
 async def progress_bar(current, total, reply, start):
     if timer.can_send():
         now = time.time()
@@ -86,12 +86,14 @@ async def progress_bar(current, total, reply, start):
             sp = str(hrb(speed)) + "/s"
             tot = hrb(total)
             cur = hrb(current)
+            
+    
             bar_length = 11
             completed_length = int(current * bar_length / total)
             remaining_length = bar_length - completed_length
-            progress_bar = "■" * completed_length + "□" * remaining_length
+            progress_bar = "▰" * completed_length + "▱" * remaining_length
             
             try:
-                await reply.edit(f'╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙✴️ : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨🕓 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙📁 : {cur}\n├ 𝙎𝙞𝙯𝙚📚 :  {tot} \n├ 𝙀𝙏𝘼⏳: {eta} \n╰────⌈ PROFESSOR ⌋───╯\n') 
+                await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰────⌈ Bholenath ⌋────╯`\n') 
             except FloodWait as e:
                 time.sleep(e.x)

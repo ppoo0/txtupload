0b94b653e730dd16d0d6710219441157e77a498b
Ekvillai9 <158746555+Ekvillai9@users.noreply.github.com>
2024-02-06 16:48:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6940355682:AAEaTbv9tLz7ZKOEcMJ1HBioIP2goQOdQSY",
-             api_id= 20544260,
-             api_hash= "a0b00461d3fba22aa186fa648d77787e")
-auth_users = [6389388334,-1002051259332]
+             bot_token= "6425963291:AAHYcrQriMyFLkyXwuAkKvBqEuiEcsKqsOU",
+             api_id= 23282918,
+             api_hash= "ccf2a6664d1ae478b8c556000f616120")
+auth_users = 5911618569,
 
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):

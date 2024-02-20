9893f61bd8ec3f0d2eba7a5ee107471cd4585bfe
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-20 22:14:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6883697340:AAH7bcXvO-c4oCr-kGl0TLsqL57cmquvovI",
+             bot_token= "6883697340:AAF_1a68Uy0nECfUWHpItzMeuQyJID4LXvc",
              api_id= 20945078,
              api_hash= "93f6b8ce4bb0ab61b4c7e42187f2aa64")
-auth_users =  [1664376941]
+auth_users =  [1664376941,6793909122]
 
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
